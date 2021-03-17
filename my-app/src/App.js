1c0383b12dad5fadc2bfbb8b6dee12d1a8ca0cc1
shivakumar901072 <shivakumar901072@gmail.com>
2021-03-17 18:12:54 +0530

    Added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,21 @@ import './App.css';
 function App() {
 
   const[posts,setPosts] = useState([]);
+  const [newPost, setNewPost] = useState("");
+
+  // const item = () => {
+  //   posts.push(newPost);
+  //   setPosts([...posts]);
+    
+  // }
 
   useEffect(() => {
     axios.get('https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0')
     .then(res => {
-      console.log(res);
+      console.log(res.data.hits[0].title)
+      console.log(res.data.hits[0].author)
+      console.log(res.data.hits[0].url)
+      console.log(res.data.hits[0].created_a)
       setPosts(res.data)
     })
     .catch(err => {
@@ -18,11 +28,18 @@ function App() {
   })
   return (
     <div >
-      {posts.hits[0].created_at}
+      {/* {posts.hits[0].created_at}
       {posts.hits[0].title}
       {posts.hits[0].author}
       {posts.hits[0].url.url}
-       
+        */}
+        <ul>
+          {posts.map(post => {
+            <li key={post.objectID}>
+              {post.hits[0].title}
+            </li>
+          })}
+        </ul>
     </div>
   );
 }
